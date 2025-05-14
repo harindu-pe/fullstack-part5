dbe24ccf4af4086e28bdc2cc891ac49e44fa9494
harindu-pe <harindu.pe@gmail.com>
2025-05-14 22:48:47 +0530

    exercise 5.7 done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? "hide" : "view"}
+        </button>
+      </div>
+
+      {visible && (
+        <div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes}</div>
+          <div>{blog.author}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
